da54a7a97b8c652e1c6282d83afaf1920a5762ae
Michael R. Cook <git@mrcook.uk>
2013-10-18 23:25:08 +0100

    added .parse(path) for easy execution

--- lib/gutenberg_rdf.rb
+++ lib/gutenberg_rdf.rb
@@ -5,4 +5,9 @@ require "gutenberg_rdf/rdf/agent"
 require "gutenberg_rdf/version"
 
 module GutenbergRdf
+
+  def self.parse(path)
+    Rdf.new(Nokogiri::XML(File.new(path)))
+  end
+
 end
---
+++ spec/gutenberg_rdf_spec.rb
@@ -0,0 +1,16 @@
+require 'spec_helper'
+
+module GutenbergRdf
+  describe ".parse" do
+    let(:file) { StringIO.new('<rdf:RDF xmlns:dcterms="http://purl.org/dc/terms/" xmlns:pgterms="http://www.gutenberg.org/2009/pgterms/" xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#"><pgterms:ebook rdf:about="ebooks/98765"/></rdf:RDF>') }
+
+    it "expects an Rdf object" do
+      File.stub(:new).and_return(file)
+      book = GutenbergRdf.parse(file)
+
+      expect(book.class).to be Rdf
+      expect(book.id).to eql '98765'
+    end
+
+  end
+end
