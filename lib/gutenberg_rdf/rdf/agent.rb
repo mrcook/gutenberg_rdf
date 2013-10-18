ec0aee22bb328e86e75ea951e06aab43ffa9f42e
Michael R. Cook <git@mrcook.uk>
2013-10-18 12:01:28 +0100

    Complete (a simple) Agent implementation

--- lib/gutenberg_rdf/rdf/agent.rb
+++ lib/gutenberg_rdf/rdf/agent.rb
@@ -11,13 +11,59 @@ module GutenbergRdf
         xml.at_xpath('pgterms:agent').attribute('about').content.match(/\A\d\d\d\d\/agents\/(\d+)\z/)[1]
       end
 
-      def last_name
-        xml.at_xpath('pgterms:agent/pgterms:name').text.split(/, */).first
+      def fullname
+        [firstname, lastname].join(' ')
       end
 
-      def first_name
-        xml.at_xpath('pgterms:agent/pgterms:name').text.split(/, */).last
+      def lastname
+        @lastname ||= name_parts[:last]
       end
+
+      def firstname
+        @firstname ||= name_parts[:first]
+      end
+
+      def birthdate
+        xml.at_xpath('pgterms:agent/pgterms:birthdate').text
+      end
+
+      def deathdate
+        xml.at_xpath('pgterms:agent/pgterms:deathdate').text
+      end
+
+      def webpage
+        xml.at_xpath('pgterms:agent/pgterms:webpage').attribute('resource').content
+      end
+
+      def aliases
+        entries = Array.new
+        xml.xpath('//pgterms:alias').each do |name|
+          entries << name.text
+        end
+        entries
+      end
+
+    private
+
+      def name_parts
+        parts = xml.at_xpath('pgterms:agent/pgterms:name').text.split(/, */)
+        last  = parts.shift
+        first = parts.reverse.join(' ')
+
+        {first: first, last: last}
+      end
+
+      # def clean_up_split_names(names)
+      #   names.map! do |name|
+      #     n = name.sub(/\(.*?\)/, '')
+      #     n.strip
+      #   end
+
+      #   names.delete 'Sir'
+      #   names.delete 'Graf'
+      #   names.delete 'graf'
+      #   names
+      # end
 
     end
   end
--- spec/gutenberg_rdf/rdf/agent_spec.rb
+++ spec/gutenberg_rdf/rdf/agent_spec.rb
@@ -23,13 +23,90 @@ module GutenbergRdf
       end
 
       it "expects the last name" do
-        expect(agent.last_name).to eql 'Doe'
+        expect(agent.lastname).to eql 'Doe'
       end
 
       it "expects the first name(s)" do
-        expect(agent.first_name).to eql 'Jon James'
+        expect(agent.firstname).to eql 'Jon James'
       end
 
+      it "expects the full name" do
+        expect(agent.fullname).to eql 'Jon James Doe'
+      end
+
+      it "expects a birth date" do
+        expect(agent.birthdate).to eql '1830'
+      end
+
+      it "expects a death date" do
+        expect(agent.deathdate).to eql '1905'
+      end
+
+      it "expects a webpage" do
+        expect(agent.webpage).to eql 'http://en.wikipedia.org/wiki/Jon_James_Doe'
+      end
+
+      it "expects any alias names" do
+        expect(agent.aliases[0]).to eql 'Doe, Jon'
+        expect(agent.aliases[1]).to eql 'Doe, J. J.'
+      end
+
+      context "when only a single name is given" do
+        let(:agent) do
+          xml = '<rdf:RDF xmlns:dcterms="http://purl.org/dc/terms/" xmlns:pgterms="http://www.gutenberg.org/2009/pgterms/" xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#">
+                   <pgterms:agent rdf:about="2009/agents/402">
+                     <pgterms:name>Dato</pgterms:name>
+                   </pgterms:agent>
+                 </rdf:RDF>'
+          rdf = Nokogiri::XML(xml)
+          Agent.new(rdf.at_xpath('rdf:RDF'))
+        end
+
+        it "expects it to be assigned to the last name" do
+          expect(agent.lastname).to eql 'Dato'
+        end
+        it "expects firstname to be an empty string" do
+          expect(agent.firstname).to eql ''
+        end
+      end
+
+      context "when the name has a suffix" do
+        let(:agent) do
+          xml = '<rdf:RDF xmlns:dcterms="http://purl.org/dc/terms/" xmlns:pgterms="http://www.gutenberg.org/2009/pgterms/" xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#">
+                   <pgterms:agent rdf:about="2009/agents/402">
+                     <pgterms:name>Doe, Jon, Sir</pgterms:name>
+                   </pgterms:agent>
+                 </rdf:RDF>'
+          rdf = Nokogiri::XML(xml)
+          Agent.new(rdf.at_xpath('rdf:RDF'))
+        end
+
+        it "expects the correct name order" do
+          expect(agent.firstname).to eql 'Sir Jon'
+          expect(agent.lastname).to eql 'Doe'
+        end
+      end
+
+      context "when full name is given in (brackets)" do
+        let(:agent) do
+          xml = '<rdf:RDF xmlns:dcterms="http://purl.org/dc/terms/" xmlns:pgterms="http://www.gutenberg.org/2009/pgterms/" xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#">
+                   <pgterms:agent agent:about="2009/agents/402">
+                     <pgterms:name>Doe, J. J. (Jon James)</pgterms:name>
+                   </pgterms:agent>
+                 </rdf:RDF>'
+          rdf = Nokogiri::XML(xml)
+          Agent.new(rdf.at_xpath('rdf:RDF'))
+        end
+
+        it "expects initials to replaced by name in brackets" do
+          pending "this functionality needs implementing"
+          expect(agent.firstname).to eql 'Jon James'
+          expect(agent.lastname).to eql 'Doe'
+        end
+        it "expects the name (excluding name in brackets) to be added to the aliases"
+        it "should not have duplicate aliases"
+      end
     end
+
   end
 end
